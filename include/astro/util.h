0d710f03cb310394d8328b0e7de5ff8ac1e91cf7
Mario Juric <mjuric@ias.edu>
2009-05-02 01:46:21 +0200

    Extrated FOREACH, FOR, ... macros to macros.h from util.h

--- include/astro/util.h
+++ include/astro/util.h
@@ -1,27 +1,7 @@
 #ifndef _astro_util_h
 #define _astro_util_h
 
-#define FOREACH2j(C, i_, x) for(C i_ = (x).begin(); i_ != (x).end(); ++i_)
-#define FOREACH2(C, x) FOREACH2j(C, i, x)
-
-#define REVEACH2j(C, i_, x) for(C i_ = (x).rbegin(); i_ != (x).rend(); ++i_)
-#define REVEACH2(C, x) REVEACH2j(C, i, x)
-
-#define FOREACHj(i_, x) for(typeof((x).begin()) i_ = (x).begin(); i_ != (x).end(); ++i_)
-#define FOREACH(x) FOREACHj(i, x)
-
-#define REVEACHj(i_, x) for(typeof((x).begin()) i_ = (x).rbegin(); i_ != (x).rend(); ++i_)
-#define REVEACH(x) REVEACHj(i, x)
-
-#define FORj(i, i0, i1) for(int i = i0; i != i1; ++i)
-#define FOR(i0, i1) FORj(i, i0, i1)
-
-#define REVj(i, i0, i1) for(int i = i0; i != i1; --i)
-#define REV(i0, i1) REVj(i, i0, i1)
-
-#define OSTREAM(T) std::ostream &operator <<(std::ostream &out, T)
-#define ISTREAM(T) std::istream &operator >>(std::istream &in, T)
-
+#include <astro/macros.h>
 #include <astro/types.h>
 #include <astro/constants.h>
 #include <cmath>
@@ -65,6 +45,8 @@ namespace util {
 	inline std::string str(size_t n) { char buf[20]; sprintf(buf, "%d", n); return buf; }
 	/// convert int to std::string
 	inline std::string str(int n) { char buf[20]; sprintf(buf, "%d", n); return buf; }
+	/// convert char to std::string
+	inline std::string str(char c) { char buf[2] = {c, 0}; return buf; }
 	/// convert double to std::string
 	inline std::string str(double n, const char *fmt = "%f") { char buf[20]; sprintf(buf, fmt, n); return buf; }
 
